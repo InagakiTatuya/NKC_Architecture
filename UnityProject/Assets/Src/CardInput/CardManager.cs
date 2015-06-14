405be2de190303895fd752251f5da602836f35f7
稲垣達也 <nkc.inagaki.tatuya@gmail.com>
2015-06-15 03:32:31 +0900

    【大幅に変更】CardInputSceneを大幅に更新。

---
+++ UnityProject/Assets/Src/CardInput/CardManager.cs
@@ -0,0 +1,213 @@
+﻿//#############################################################################
+//  社員証を管理するクラス
+//  作者：稲垣達也
+//#############################################################################
+
+//名前空間/////////////////////////////////////////////////////////////////////
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+//クラス///////////////////////////////////////////////////////////////////////
+public class CardManager : MonoBehaviour {
+    //定数^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    //CardPackの生成数
+    private const int CARDPACK_MAX = (Database.PLAYER_MAX_COUNT - 1) / 3 + 1;
+    //Cardの数／Cardの最大要素数
+    private const int CARD_MAX     = CARDPACK_MAX * 3;
+    
+    //リソースの参照^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    private GameObject  CARDPACK_PREFAB;    //社員証パック(CardPack)のプレハブ
+
+    //統括しているシステム^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    private CardInputSystem ciSystem;  //このシーンを統括するSystem
+    
+    //管理データ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    private Card[]      m_Card;             //社員証
+    private int         m_PlayerCount;      //参加するプレイヤー数
+    
+    
+
+    //非公開関数///////////////////////////////////////////////////////////////
+    //初期化===================================================================
+    void Awake() {
+        //プレハブ読み込み-----------------------------------------------------
+        CARDPACK_PREFAB =
+            Resources.Load<GameObject>("Prefab/CardInput/CardPack");
+    }
+
+    void Start() {
+        //システムの参照-------------------------------------------------------
+        ciSystem = GameObject.Find("System").GetComponent<CardInputSystem>();
+
+        //CardPackを生成と初期化-----------------------------------------------
+        for(int i=0; i < CARDPACK_MAX; i++) {
+            //生成・親設定
+            Transform tra = Instantiate<GameObject>(CARDPACK_PREFAB).transform;
+            tra.SetParent(this.transform, false);
+        }
+
+        //Card初期化-----------------------------------------------------------
+        m_Card = new Card[CARD_MAX];
+        int index = 0;
+        foreach(Transform cpTra in transform) {
+            //Cardコンポーネット取得- - - - - - - - - - - - - - - - - - - - - -
+            m_Card[index + 0] = cpTra.GetChild(0).GetComponent<Card>();
+            m_Card[index + 1] = cpTra.GetChild(1).GetComponent<Card>();
+            m_Card[index + 2] = cpTra.GetChild(2).GetComponent<Card>();
+            //IndexNoを設定 - - - - - - - - - - - - - - - - - - - - - - - - - -
+            m_Card[index + 0].Init( index + 0, OnFhotoEnter, OnEndEdit, OnValueChange );
+            m_Card[index + 1].Init( index + 1, OnFhotoEnter, OnEndEdit, OnValueChange );
+            m_Card[index + 2].Init( index + 2, OnFhotoEnter, OnEndEdit, OnValueChange );
+
+            //要素数加算- - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            index += 3;
+        }
+
+        //アクティブ設定-------------------------------------------------------
+        CardsSetActive(1, true);//一つ目だけtrue
+
+        //プレイヤー数---------------------------------------------------------
+        m_PlayerCount = 1;
+    }
+
+    //更新=====================================================================
+    void Update() {
+
+    }
+
+    //社員証のアクティブ設定===================================================
+    //  Card と CardPack のアクティブの設定を行う
+    //------------------------------------------------------------------------
+    private void CardsSetActive(int _Length, bool _Active = true) {
+        //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+        //  配列の先頭からすべてアクティブにする。
+        //  重いようであれば変更する。
+        //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
+        //配列先頭から_Lengthまでは、_Activeを渡す
+        int index = 0;
+        for(/**/; index < _Length; index++) {
+            if(index % 3 == 0) {
+                transform.GetChild(index / 3).gameObject.SetActive(_Active);
+            }
+            m_Card[index].gameObject.SetActive(_Active);
+        }
+        //_Leingth 以下のものは !_Active を渡す
+        for(/**/; index < CARD_MAX; index++) {
+            if(index % 3 == 0) {
+                transform.GetChild(index / 3).gameObject.SetActive(!_Active);
+            }
+            m_Card[index].gameObject.SetActive(!_Active);
+        }
+        
+    }
+
+    //イベント=================================================================
+
+    //完了ボタン===============================================================
+    //  タイミング：ＯＫボタンがタップ瞬間。
+    //    Databaseのフォーマットにあわせデータを作り、Databaseに入れる。
+    //------------------------------------------------------------------------
+    public void OnAppButtonEnter() {
+        //デバック用=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=
+        #if UNITY_EDITOR 
+        Debug.Log(" Time:"+Time.time.ToString("0.00") + " - " +
+            this.GetType().Name + " - " +
+            System.Reflection.MethodBase.GetCurrentMethod().Name + "\n" +
+            "State="+(ciSystem.getState == CardInputSystem.STATE_USUALLY) +
+            " PlyCount="+(m_PlayerCount < Database.PLAYER_MAX_COUNT) );
+        #endif
+        //=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=
+        
+        //ステートが通常時以外は、処理しない
+        if(ciSystem.getState != CardInputSystem.STATE_USUALLY) return;
+        //プレイヤーがいない場合、メッセージ処理をする
+        if(m_PlayerCount >= Database.PLAYER_MAX_COUNT) {
+            //=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+            //  ここに"社員がいません。"メッセージを出す処理
+            //  または、そのフラグ。
+            //=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#
+            return;
+        }
+
+        //Databaseに渡すデータを作る-------------------------------------------
+        StractPlayerData[] data = new StractPlayerData[m_PlayerCount];
+        for(int i = 0; i < m_PlayerCount; i++) {
+            data[i] = m_Card[i].data;
+        }
+        //Databaseに渡す-------------------------------------------------------
+        Database.obj.SetPlyaerDatas(ref data);
+
+
+    }
+
+    //社員追加ボタン===========================================================
+    //  タイミング：社員追加ボタンがタップ瞬間。
+    //    プレイヤーデータを追加し、参加者数を増やす。
+    //------------------------------------------------------------------------
+    public void OnPlayerAddButtonEnter() {
+        //ステートが通常時以外は、処理しない
+        if(ciSystem.getState != CardInputSystem.STATE_USUALLY) return;
+        //プレイヤー数が最大値に達していたら処理しない
+        if(m_PlayerCount >= Database.PLAYER_MAX_COUNT) return;
+
+        m_PlayerCount++;                //プレイヤー数加算
+        CardsSetActive(m_PlayerCount);  //アクティブの操作
+    }
+
+    //社員証の削除ボタン=======================================================
+    //  タイミング：社員証削除ボタンがタップされた瞬間。
+    //    プレイヤーデータを削除し、参加者数を減らす。
+    //------------------------------------------------------------------------
+    public void OnPlayerRemoveButtonEnter(int _cardIndex) {
+
+    }
+
+    //写真を変更===============================================================
+    //  タイミング：写真がタップされた瞬間。
+    //    写真選択ウィンドウを出す
+    //-------------------------------------------------------------------------
+    public void OnFhotoEnter(int _cardIndex) {
+        //デバック用=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=
+        #if UNITY_EDITOR
+        Debug.Log(" Time:" + Time.time.ToString("0.00") + " - " +
+            this.GetType().Name + " - " +
+            System.Reflection.MethodBase.GetCurrentMethod().Name +
+            "\nCardIndex = " + _cardIndex);
+        #endif
+        //=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=
+        //写真選択ウィンドウを出す---------------------------------------------
+        //=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+        //  ここに写真選択ウィンドウを出す処理
+        //=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+    }
+
+    //名前入力終了=============================================================
+    //  タイミング：入力が終えたら呼び出される。
+    //    CardのDataに入力された値を入れる。
+    //-------------------------------------------------------------------------
+    public void OnEndEdit(int _cardIndex) {
+        //デバック用=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=
+        #if UNITY_EDITOR 
+        Debug.Log(" Time:"+Time.time.ToString("0.00") + " - " +
+            this.GetType().Name + " - " +
+            System.Reflection.MethodBase.GetCurrentMethod().Name +
+            "\nCardIndex = " + _cardIndex);
+        #endif
+        //=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=\=
+
+        //CardのDataに入力された値を入れる-------------------------------------
+        m_Card[_cardIndex].DataSetName();
+     
+    }
+
+    //名前入力中===============================================================
+    //  タイミング：入力するたびに呼ばれる。
+    //    今のところ特に処理することはない。
+    //    演出の強化に使えたらいいなぁ
+    //-------------------------------------------------------------------------
+    public void OnValueChange(int _cardIndex) { }
+
+}
--- UnityProject/Assets/Src/Database.cs
+++ UnityProject/Assets/Src/Database.cs
@@ -6,21 +6,30 @@
 //名前空間/////////////////////////////////////////////////////////////////////
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 //クラス///////////////////////////////////////////////////////////////////////
 public class Database : SingletonCustom<Database> {
 
+    //ステージ情報^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     private int m_StageId;
     public  int StageId { 
         get{ return m_StageId;  } 
         set{ m_StageId = value; }
     }
 
-
+    //プレイヤー情報^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    public  const int          PLAYER_MAX_COUNT = 15; //参加できる数
+    private StractPlayerData[] m_PlayerDatas;         //プレイヤーデータ
+    
+    public  int                PlayerCount{ get{ return m_PlayerDatas.Length; } }
+    public  StractPlayerData[] PlayerData { get{ return m_PlayerDatas;        } }
+    
     //初期化===================================================================
-    void Awak() {
+    void Awake() {
         BaseAwake(this); //シングルトンの設定をする
+
+        Debug.Log("Database.awake()");
     }
 
 	void Start () {
@@ -31,5 +40,15 @@ public class Database : SingletonCustom<Database> {
 	void Update () {
 	
 	}
+
+    //公開関数=================================================================
+    
+    //プレイヤーデータを入れる=================================================
+    //  CardInputシーンで作られたデータを保存
+    //-------------------------------------------------------------------------
+    public void SetPlyaerDatas(ref StractPlayerData[] _datas) {
+        m_PlayerDatas = new StractPlayerData[_datas.Length];
+        _datas.CopyTo(this.m_PlayerDatas, 0);
+    }
 
 }
