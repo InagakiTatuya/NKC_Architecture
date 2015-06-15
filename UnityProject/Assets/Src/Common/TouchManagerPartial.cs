6ae3f51d233019c1fa9393af89b816b7fb9a95fc
田辺 純也 <nkc.tanabe.jyunnya@gmail.com>
2015-06-15 10:21:51 +0900

    【追加】GameSceneとPauseSceneを追加

---
+++ UnityProject/Assets/Src/Common/TouchManagerPartial.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial interface TouchManagerPartial
+{
+	// タッチした瞬間に呼ばれる。
+	void TouchDownOnce();
+	// タッチしたときに呼ばれる。
+	void TouchDown();
+	// スライドしたときに呼ばれる。
+	void TouchMove();
+	// タッチを離したときに呼ばれる。
+	void TouchUp(bool b);
+}
