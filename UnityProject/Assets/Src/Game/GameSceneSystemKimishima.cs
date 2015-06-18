a44e0b3e581d60579b871cab74ffe46ceb220e1d
NKC君島一刀 <nkc.kazuto.kimishima@gmail.com>
2015-06-18 12:49:03 +0900

    パーツ選択ウィンドウのステート遷移の仕組みをつくりました

--- UnityProject/Assets/Src/Game/GameSceneSystemKimishima.cs
+++ UnityProject/Assets/Src/Game/GameSceneSystemKimishima.cs
@@ -11,6 +11,7 @@
 
 //名前空間//////////////////////////////////////////////////
 using	UnityEngine;
+using	UnityEngine.Events;
 using	UnityEngine.UI;
 using	System.Collections;
 
@@ -18,17 +19,38 @@ using	System.Collections;
 //ゲームシーンのシステム_Begin//----------------------------
 public	partial class GameSceneSystem : MonoBehaviour{
 
+	//列挙・テーブル//////////////////////////////////////////
+	enum	PartsSelectWindowStateNo{//ステート番号_Begin//--
+		Open,		//開く
+		Neutral,	//通常
+		Close,		//閉じる
+		Hide,		//隠れている
+		Length,		//長さ
+	}//ステート番号_End//------------------------------------
+
+	private	UnityAction[]	partsSelectWindowUpdateFunc	= null;
 	//変数//////////////////////////////////////////////////
 	private	Vector2	FLOORWINDOW_POS	= new Vector2(-192.0f,-384.0f);
 
+	//フロア関連
 	private	int		floor;
 	private	Image	floorWindow	= null;
 	private	Text	floorText	= null;
 	private	Vector2	floorSize;
 
+	//パーツ選択関連
+	private	int		partsSelectWindowStateNo;
+	private	int		partsSelectWindowStateTime;
+
 	//初期化////////////////////////////////////////////////
 	//俺の初期化関数_Begin//--------------------------------
 	private	void	StartKimishimaSystem(){
+		partsSelectWindowUpdateFunc	= new UnityAction[]{
+			this.UpdatePartsSelectWindowOpen,
+			this.UpdatePartsSelectWindowNeutral,
+			this.UpdatePartsSelectWindowClose,
+			null,
+		};
 		floor		= 1;
 		floorSize	= new Vector2(128.0f,128.0f);
 		StartKimishimaSystemCreateFloorWindow();
@@ -37,25 +59,30 @@ public	partial class GameSceneSystem : MonoBehaviour{
 
 	//階層ウィンドウを生成_Begin//--------------------------
 	private	void	StartKimishimaSystemCreateFloorWindow(){
-		GameObject	obj	= TitleSystem.CreateObujectInCanvas("Prefab/Game/FloorWindow",canvasObject);
-		floorWindow	= obj.GetComponent<Image>();
+		GameObject	obj		= TitleSystem.CreateObujectInCanvas("Prefab/Game/FloorWindow",canvasObject);
+		floorWindow			= obj.GetComponent<Image>();
 		floorWindow.rectTransform.localPosition	= FLOORWINDOW_POS;
+		floorWindow.color	= new Color(0.0f,0.5f,0.5f,1.0f);
 	}//階層ウィンドウを生成_End//---------------------------
 
 	//階層テキストを生成_Begin//----------------------------
 	private	void	StartKimishimaSystemCreateFloorText(){
-		GameObject	obj	= TitleSystem.CreateObujectInCanvas("Prefab/Select/Text",canvasObject);
-		floorText	= obj.GetComponent<Text>();
+		GameObject	obj		= TitleSystem.CreateObujectInCanvas("Prefab/Select/Text",canvasObject);
+		floorText			= obj.GetComponent<Text>();
 		floorText.rectTransform.localPosition	= FLOORWINDOW_POS;
 		floorText.text		= "1\n階層目";
 		floorText.fontSize	= 48;
 		floorText.color		= Color.white;
 	}//階層テキストを生成_End//-----------------------------
-	
+
 	//更新//////////////////////////////////////////////////
 	//俺の更新関数_Begin//----------------------------------
 	private	void	UpdateKimishimaSystem(){
+		if(partsSelectWindowStateNo < 0 || partsSelectWindowStateNo >= (int)PartsSelectWindowStateNo.Length)
+			ChangePartsSelectWindowStateNo(PartsSelectWindowStateNo.Hide,true);
 		UpdateFloorWindow();
+		if(partsSelectWindowUpdateFunc[partsSelectWindowStateNo] != null)
+			partsSelectWindowUpdateFunc[partsSelectWindowStateNo]();
 	}//俺の更新関数_End//-----------------------------------
 
 	//階層ウィンドウを更新_Begin//--------------------------
@@ -64,6 +91,18 @@ public	partial class GameSceneSystem : MonoBehaviour{
 		if(floorText != null)	floorText.rectTransform.sizeDelta	= floorSize;
 	}//階層ウィンドウを更新_End//---------------------------
 
+	//ウィンドウを開く_Beign//------------------------------
+	private	void	UpdatePartsSelectWindowOpen(){
+	}//ウィンドウを開く_End//-------------------------------
+
+	//待機_Beign//-----------------------------------------
+	private	void	UpdatePartsSelectWindowNeutral(){
+	}//待機_End//------------------------------------------
+
+	//ウィンドウを閉じる_Beign//-----------------------------
+	private	void	UpdatePartsSelectWindowClose(){
+	}//ウィンドウを閉じる_End//------------------------------
+
 	//その他関数////////////////////////////////////////////
 	/// <summary>フロアを上げていく</summary>
 	private	void	AddFloor(){//階層を進める_Begin//-------
@@ -71,4 +110,23 @@ public	partial class GameSceneSystem : MonoBehaviour{
 		floorText.text	= "" + floor + "\n階層目";
 	}//階層を進める_End//-----------------------------------
 
-}//ゲームシーンのシステム_End//-----------------------------
+	/// <summary>パーツ選択ウィンドウを表示</summary>_Begin//-
+	private	void	OpenPartsSelectWindow(){
+		ChangePartsSelectWindowStateNo(PartsSelectWindowStateNo.Open);
+	}//パーツ選択ウィンドウを表示_End//-----------------------
+
+	//パーツ選択ウィンドウのステート番号を変更する_Begin//------
+	private	void	ChangePartsSelectWindowStateNo(
+		PartsSelectWindowStateNo stateNo,bool overrapFlg = false){
+		int		value	= (int)stateNo;
+		if(!overrapFlg && value == partsSelectWindowStateNo)	return;
+		partsSelectWindowStateNo	= value;
+		partsSelectWindowStateTime	= -1;
+	}//パーツ選択ウィンドウのステート番号を変更する_End//-------
+
+	//パーツ選択ウィンドウを生成_Beign//---------------------
+	private	void	CreatePartsSelectWindow(){
+
+	}//パーツ選択ウィンドウを生成_End//----------------------
+
+}//ゲームシーンのシステム_End//------------------------------
