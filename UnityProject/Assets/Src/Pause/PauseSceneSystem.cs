781c109115c5fd685719fa2e78dbd50de60a7def
田辺 純也 <nkc.tanabe.jyunnya@gmail.com>
2015-06-18 12:56:25 +0900

    【完成】 PauseScene完成しました。

--- UnityProject/Assets/Src/Pause/PauseSceneSystem.cs
+++ UnityProject/Assets/Src/Pause/PauseSceneSystem.cs
@@ -1,57 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PauseSceneSystem : BaseObject, TouchManagerPartial
+public class PauseSceneSystem : MonoBehaviour
 {
-	private const int SCREEN_WIDTH = 361;
-	private const int SCREEN_HEIGHT = 637;
-	private const float SCREEN_PULLDOWN = 26.0f / 637.0f;
-	public Vector2 scale;
-	public float pos;
-
-	/*
-	361 673	611
-	364 582 589
-	*/
-
-	protected override void AwakeEx()
+	private string[] sceneName = new string[]
 	{
+		"Game",
+		"Select"
+	};
+
+	private void Awake()
+	{
+		gameObject.SetActive(false);
 	}
 
-	protected override void StartEx()
+	public void CallRetryScene()
 	{
+		Application.LoadLevel(sceneName[0]);
 	}
-
-	protected override void UpdateEx()
+	public void CallSelectScene()
 	{
-		scale.x = (float)Screen.width / SCREEN_WIDTH;
-		scale.y = (float)Screen.height / SCREEN_HEIGHT;
-		pos = Screen.height - Screen.height * SCREEN_PULLDOWN;
-
-		traPos = new Vector3(traPos.x, pos, traPos.z);
+		Application.LoadLevel(sceneName[1]);
 	}
-
-	protected override void FixedUpdateEx()
+	public void CallPauseGUI()
 	{
-	}
-
-	protected override void LateUpdateEx()
-	{
-	}
-
-	public void TouchDownOnce()
-	{
-	}
-
-	public void TouchDown()
-	{
-	}
-
-	public void TouchMove()
-	{
-	}
-
-	public void TouchUp(bool b)
-	{
+		gameObject.SetActive(!gameObject.activeSelf);
 	}
 }
