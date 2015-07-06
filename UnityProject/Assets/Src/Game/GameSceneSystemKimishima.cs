0e5945b99aedce4823398925d842e4bc1f864445
NKC君島一刀 <nkc.kazuto.kimishima@gmail.com>
2015-07-06 12:13:25 +0900

    更新

--- UnityProject/Assets/Src/Game/GameSceneSystemKimishima.cs
+++ UnityProject/Assets/Src/Game/GameSceneSystemKimishima.cs
@@ -289,8 +289,4 @@ public	partial class GameSceneSystem : MonoBehaviour{
 #endif
 	}//押されたボタンのIDを受け取る_End//-------------------
 
-	//チェック用の関数
-	private void UpdateCheckKimishima(){
-	}
-
 }//ゲームシーンのシステム_End//------------------------------
---
+++ UnityProject/Assets/Src/Game/GameSceneSystemKimishimaCheck.cs
@@ -0,0 +1,41 @@
+﻿//----------------------------------------------------------
+//ゲームシーンのシステム
+//更新日 :	06 / 29 / 2015
+//更新者 :	君島一刀
+//----------------------------------------------------------
+
+//プリプロセッサ////////////////////////////////////////////
+#if UNITY_EDITOR
+#define	DEBUG_GAMESCENE
+#endif
+
+//名前空間//////////////////////////////////////////////////
+using	UnityEngine;
+using	UnityEngine.Events;
+using	UnityEngine.UI;
+using	System.Collections;
+
+//クラス////////////////////////////////////////////////////
+//ゲームシーンのシステム_Begin//----------------------------
+public	partial class GameSceneSystem : MonoBehaviour{
+
+	//変数//////////////////////////////////////////////////
+	GameObject	beginObject	= null;
+	GameObject	endObject	= null;
+
+	//更新//////////////////////////////////////////////////
+	//チェック用の関数_Begin//------------------------------
+	private void UpdateCheckKimishima(){
+		if(beginObject != null && endObject != null){
+			Vector2	beginPos	= beginObject.transform.position;
+			Vector2	endPos		= endObject.transform.position;
+			float	n			= beginPos.x * endPos.x + beginPos.y * endPos.y;
+			float	rad			= Mathf.Acos(n);
+		}
+		if(stateTime >= 3.0f){
+			ChangeState(StateNo.CardView);
+			return;
+		}
+	}//チェック用の関数_End//-------------------------------
+
+}//ゲームシーンのシステム_End//-----------------------------
