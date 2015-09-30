89477bbb743623471e0a1bbfd31c764a483cc90c
NKC君島一刀 <nkc.kazuto.kimishima@gmail.com>
2015-09-30 11:26:17 +0900

    カメラの上昇バグ直ったくさいのでコミット

--- UnityProject/Assets/Src/Game/Kimishima/CameraMove.cs
+++ UnityProject/Assets/Src/Game/Kimishima/CameraMove.cs
@@ -98,17 +98,23 @@ public class CameraMove : MonoBehaviour {
 		int		flg		= 0;
 		float	speed	= 0.0f;
 		for(int i = 0;i < touchCount - 1;i ++){
+			//タッチ情報を取得
+			Touch	iTouch	= Input.touches[i];
+			if(iTouch.deltaTime == 0.0f)	continue;
 			for(int j = i + 1;j < touchCount;j ++){
-				Touch	iTouch	= Input.touches[i];
+				//タッチ情報を取得
 				Touch	jTouch	= Input.touches[j];
-				if(iTouch.deltaTime == 0.0f)	return;
-				if(jTouch.deltaTime == 0.0f)	return;
+				if(jTouch.deltaTime == 0.0f)	continue;
+				//座標の差を取得
 				int		ix		= (iTouch.deltaPosition.x < 0.0f)?-1:((iTouch.deltaPosition.x > 0.0f)?1:0);
 				int		jx		= (jTouch.deltaPosition.x < 0.0f)?-1:((jTouch.deltaPosition.x > 0.0f)?1:0);
 				int		iy		= (iTouch.deltaPosition.y < 0.0f)?-1:((iTouch.deltaPosition.y > 0.0f)?1:0);
 				int		jy		= (jTouch.deltaPosition.y < 0.0f)?-1:((jTouch.deltaPosition.y > 0.0f)?1:0);
-				speed	= Mathf.Abs(iTouch.deltaPosition.magnitude / iTouch.deltaTime) + Mathf.Abs(jTouch.deltaPosition.magnitude / jTouch.deltaTime);
+				//スピードを計算
+				speed	= Mathf.Abs(iTouch.deltaPosition.magnitude / iTouch.deltaTime)
+						+ Mathf.Abs(jTouch.deltaPosition.magnitude / jTouch.deltaTime);
 				speed	/= Screen.width;
+				//移動量を確認
 				if(ix != 0 && jx != 0 && ix == jx && iy != 0 && jy != 0 && iy == jy)	continue;
 				if(ix > jx){
 					if(iTouch.position.x < jTouch.position.x)	flg	=  1;
@@ -128,9 +134,6 @@ public class CameraMove : MonoBehaviour {
 			}
 			if(flg != 0)	break;
 		}
-		float	fieldOfView		= Camera.main.fieldOfView + speed * flg;
-		if(fieldOfView < 30.0f)		fieldOfView	= 30.0f;
-		if(fieldOfView > 120.0f)	fieldOfView	= 120.0f;
-		Camera.main.fieldOfView	= fieldOfView;
+		Camera.main.fieldOfView	= Mathf.Max(Mathf.Min(Camera.main.fieldOfView + speed * flg,120.0f),30.0f);
 	}
 }
