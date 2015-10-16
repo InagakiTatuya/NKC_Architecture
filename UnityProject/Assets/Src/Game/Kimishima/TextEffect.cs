583ea6a404ec3591fd5dc46b2cd94d8dca9d8dcd
NKC君島一刀 <nkc.kazuto.kimishima@gmail.com>
2015-10-16 13:16:31 +0900

    間違った修正を修正

--- UnityProject/Assets/Src/Game/Kimishima/TextEffect.cs
+++ UnityProject/Assets/Src/Game/Kimishima/TextEffect.cs
@@ -10,19 +10,60 @@ using System.Collections;
 //クラス//-----------------------------------------
 public class TextEffect : MonoBehaviour {
 
-	//変数//---------------------------------------
-
-
-	//初期化//-------------------------------------
-	void Start () {
-
+	//列挙//------------------------------------------
+	public	enum ID{
+		Bon_bo,			//ボンのボ
+		Bon_n,			//ボンのン
+		Gura_gu,		//グラのグ
+		Gura_ra,		//グラのラ
+		Gassharn_ga,	//ガッシャーン!!のガ
+		Gassharn_tsu,	//ガッシャーン!!のッ
+		Gassharn_shi,	//ガッシャーン!!のシ
+		Gassharn_ya,	//ガッシャーン!!のャ
+		Gassharn_haihun,//ガッシャーン!!のー
+		Gassharn_n,		//ガッシャーン!!のン
+		Gassharn_ex,	//ガッシャーン!!の!!
 	}
 	
-	//更新//---------------------------------------
-	void Update () {
-
+	//変数//------------------------------------------
+	private	Material	material		= null;
+	private	Vector2		tiling			= Vector2.one;
+	private	Vector2		offset			= Vector2.zero;
+	private	Color		CharaColor		= Color.black;
+	private	Color		OutlineColor	= Color.white;
+	
+	public	int			id;
+	public	Texture2D	texture;
+	public	GameObject	targetObject;
+	public	Vector3		pos;
+	private	Vector3		scale;
+	private	Quaternion	rotate;
+	
+	//初期化//----------------------------------------
+	void Start () {
+		StartGetMaterial();
 	}
-	void	UpdateBon(){
-
+	private	void	StartGetMaterial(){//マテリアルを読み込む
+		Renderer	renderer	= GetComponent<Renderer>();
+		material	= renderer.material;
+	}
+	
+	//更新//------------------------------------------
+	void Update () {
+		if(material == null)	return;
+		UpdateMaterial();
+		UpdateTranceform();
+	}
+	private	void	UpdateMaterial(){//マテリアル関連を更新
+		material.SetTexture			("_MainTex",texture);
+		material.SetTextureScale	("_MainTex",tiling);
+		material.SetTextureOffset	("_MainTex",offset);
+	}
+	private	void	UpdateTranceform(){//オブジェクトステータスを更新
+		Vector3		dist	= targetObject.transform.position - pos;
+		Quaternion	lookAt	= Quaternion.LookRotation(dist,Vector3.up);
+		transform.position		= pos;
+		transform.localScale	= scale;
+		transform.rotation		= lookAt * rotate;
 	}
 }
