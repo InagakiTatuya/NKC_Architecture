286f0419cf9ddd5987aaf83f6d790862c522d3e1
田辺 純也 <nkc.tanabe.jyunnya@gmail.com>
2016-01-14 12:39:25 +0900

    【更新】 ボタンのスケーリングアニメーションを追加

---
+++ UnityProject/Assets/Src/Common/ButtonScale.cs
@@ -0,0 +1,54 @@
+﻿//名前空間//////////////////////////////////////////////////
+using	UnityEngine;
+using	UnityEngine.Events;
+using	UnityEngine.UI;
+using	UnityEngine.EventSystems;
+using	System.Collections;
+
+//クラス////////////////////////////////////////////////////
+//ボタンのシステム_Begin//----------------------------------
+public class ButtonScale : MonoBehaviour,IPointerUpHandler,IPointerDownHandler,IDragHandler {
+
+//パブリックフィールド//------------------------------------
+
+	//変数//////////////////////////////////////////////////
+
+	private	Vector2		buttonSize;
+	private Image		image;
+
+	private	Vector2		size;
+
+	//ステートチェックプロパティ
+	private	bool		f_press;
+	public	bool		press{get{return	f_press;}}
+
+	//初期化////////////////////////////////////////////////
+	public	void	Start () {//初期化
+		StartButton();
+		size			= buttonSize;
+	}
+	private	void	StartButton(){//ボタンを初期化
+		image	= GetComponent<Image>();
+		buttonSize = image.rectTransform.localScale;
+	}
+
+	//更新//////////////////////////////////////////////////
+	public	void	Update(){//更新_Beign//-----------------
+		image.rectTransform.localScale		= size;
+		if(f_press)	size	= size * 0.5f + buttonSize * 0.4f;
+		else 		size	= size * 0.5f + buttonSize * 0.5f;
+	}//更新_End//-------------------------------------------
+
+	//その他関数///////////////////////////////////////////
+	//コールバック関数を設定_Beign//------------------------
+	public	void	OnPointerDown(PointerEventData eventData){
+		f_press	= true;
+	}
+	public	void	OnDrag(PointerEventData eventData){
+	}
+	public	void	OnPointerUp(PointerEventData eventData){
+		f_press	= false;
+	}
+	//コールバック関数を設定_Beign//------------------------
+
+}//ボタンのシステム_End//-----------------------------------
---
+++ UnityProject/Assets/Src/Common/ImageButtonScale.cs
@@ -0,0 +1,74 @@
+﻿//名前空間//////////////////////////////////////////////////
+using	UnityEngine;
+using	UnityEngine.Events;
+using	UnityEngine.UI;
+using	UnityEngine.EventSystems;
+using	System.Collections;
+
+//クラス////////////////////////////////////////////////////
+//ボタンのシステム_Begin//----------------------------------
+public class ImageButtonScale : MonoBehaviour,IPointerUpHandler,IPointerDownHandler,IDragHandler {
+
+//パブリックフィールド//------------------------------------
+
+	//変数//////////////////////////////////////////////////
+
+	private	Vector2		buttonPos;
+	private	Vector2		buttonSize;
+	private	Vector2		buttonTempSize;
+	private Image		button;
+
+	private	Vector2		imagePos;
+	private	Vector2		imageSize;
+	private	Vector2		imageTempSize;
+	private Image		image;
+
+
+	//ステートチェックプロパティ
+	private	bool		f_press;
+	public	bool		press{get{return	f_press;}}
+
+	//初期化////////////////////////////////////////////////
+	public	void	Start () {//初期化
+		StartButton();
+		StartButtonImage();
+	}
+	private	void	StartButton(){//ボタンを初期化
+		button	= GetComponent<Image>();
+		buttonPos = button.rectTransform.localPosition;
+		buttonSize = button.rectTransform.localScale;
+		buttonTempSize = buttonSize;
+	}
+	private	void	StartButtonImage(){//ボタンを初期化
+		image	= transform.GetChild(0).GetComponent<Image>();
+		imagePos = image.rectTransform.localPosition;
+		imageSize = image.rectTransform.localScale;
+		imageTempSize = imageSize;
+	}
+
+	//更新//////////////////////////////////////////////////
+	public	void	Update(){//更新_Beign//-----------------
+		image.rectTransform.localScale		= imageTempSize;
+		button.rectTransform.localScale		= buttonTempSize;
+		if(f_press)	{
+			imageTempSize	= imageTempSize * 0.5f + imageSize * 0.4f;
+			buttonTempSize	= buttonTempSize * 0.5f + buttonSize * 0.4f;
+		}else{
+			imageTempSize	= imageTempSize * 0.5f + imageSize * 0.5f;
+			buttonTempSize	= buttonTempSize * 0.5f + buttonSize * 0.5f;
+		}
+	}//更新_End//-------------------------------------------
+
+	//その他関数///////////////////////////////////////////
+	//コールバック関数を設定_Beign//------------------------
+	public	void	OnPointerDown(PointerEventData eventData){
+		f_press	= true;
+	}
+	public	void	OnDrag(PointerEventData eventData){
+	}
+	public	void	OnPointerUp(PointerEventData eventData){
+		f_press	= false;
+	}
+	//コールバック関数を設定_Beign//------------------------
+
+}//ボタンのシステム_End//-----------------------------------
